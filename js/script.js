cd1fb7d23d6bc2b6c5256c9019cf0413e686345f
Alejandro Martín Cancelos <alcancelos@gmail.com>
2021-10-26 17:23:24 -0300

    galeria

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function MostrarFoto() {
+    var x = document.getElementById("Ampliacion1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
